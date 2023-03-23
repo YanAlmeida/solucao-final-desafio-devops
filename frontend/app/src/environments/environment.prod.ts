9cf9d05a3b8eec95a2cd1ad3c83f8aee78ae9c7d
Yan Almeida <yan.almeida91@hotmail.com>
2023-03-22 23:22:48 -0300

    env: inclusao de codigo do front com alteracao no recebimento de variaveis de ambiente

---
+++ frontend/app/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiURL: window["env"]["apiUrl"] || "0.0.0.0:8080",
+  clientId: 'my-angular-app',
+  clientSecret: '@321',
+  obterTokenUrl: '/oauth/token'
+};
